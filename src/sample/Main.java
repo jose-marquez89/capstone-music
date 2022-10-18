f598dc43e808cb8bb1a10f891368dcb7652c71a6
jose-marquez89 <jlmarquez89@gmail.com>
2022-10-18 08:40:56 -0500

    adds one more test for vm

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("This is going to be the performance assessment");
+        primaryStage.setTitle("Scheduler");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
