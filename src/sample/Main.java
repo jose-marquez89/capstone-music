3e70f721de9f234c80f3974f9f114f19d7b247e1
jose-marquez89 <jlmarquez89@gmail.com>
2022-10-21 18:23:54 -0400

    makes some changes to default files for run config

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     // this method will be rewritten
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/sample.fxml"));
         primaryStage.setTitle("Scheduler");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
