92fef98b8b709f2892f369f49bd9a37269275bce
jose-marquez89 <jlmarquez89@gmail.com>
2022-10-21 19:19:07 -0400

    breaks up user class constructor arguments

--- src/sample/model/User.java
+++ src/sample/model/User.java
@@ -5,7 +5,8 @@ import java.time.ZonedDateTime;
 public class User extends Person {
     private String password;
 
-    public User(int id, String name, ZonedDateTime createDate, String createdBy, ZonedDateTime lastUpdate, String lastUpdatedBy, String password) {
+    public User(int id, String name, ZonedDateTime createDate, String createdBy,
+                ZonedDateTime lastUpdate, String lastUpdatedBy, String password) {
         super(id, name, createDate, createdBy, lastUpdate, lastUpdatedBy);
         this.password = password;
     }
