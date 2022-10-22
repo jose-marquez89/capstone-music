5f0e8589bb040bcf1ab2303db87343b0f4e2b36c
jose-marquez89 <jlmarquez89@gmail.com>
2022-10-22 15:01:09 -0400

    adds initial code for login form controller

--- src/sample/controller/LogInController.java
+++ src/sample/controller/LogInController.java
@@ -1,4 +1,18 @@
 package sample.controller;
 
+import javafx.event.ActionEvent;
+
+import java.awt.*;
+
 public class LogInController {
+    private TextField usernameField;
+    private TextField passwordField;
+    private String candidateUser;
+    public void logIn(ActionEvent event) {
+        candidateUser = usernameField.getText();
+
+        // query database for this user
+            // if result is null display invalid user/password message
+            // else compare entered password with password from entered user
+    }
 }
