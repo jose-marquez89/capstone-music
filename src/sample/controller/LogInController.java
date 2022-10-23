d013bbe77e2db021f4c148b58e5ea480c6a64f93
jose-marquez89 <jlmarquez89@gmail.com>
2022-10-23 09:03:53 -0400

    adds initial scaffold for user validation

--- src/sample/controller/LogInController.java
+++ src/sample/controller/LogInController.java
@@ -1,18 +1,30 @@
 package sample.controller;
 
 import javafx.event.ActionEvent;
+import sample.dao.DBConnector;
+import sample.dao.Query;
 
 import java.awt.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class LogInController {
     private TextField usernameField;
     private TextField passwordField;
+    private ResultSet queryResult;
     private String candidateUser;
-    public void logIn(ActionEvent event) {
+    public void logIn(ActionEvent event) throws SQLException {
+        String enteredPword = passwordField.getText();
         candidateUser = usernameField.getText();
 
-        // query database for this user
-            // if result is null display invalid user/password message
-            // else compare entered password with password from entered user
+        // TODO: complete user authentication flow
+        DBConnector.connect();
+        Query.runQuery("SELECT user_name, password FROM users WHERE user_name = '" + candidateUser + "';");
+        queryResult = Query.getResults();
+
+        if (queryResult.first()) {
+            // TODO: compare entered password to database password
+        }
+        // TODO: set logic for log-in failure
     }
 }
