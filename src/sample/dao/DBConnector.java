b96a585c423acc956c19381d03eb65177211f78d
jose-marquez89 <jlmarquez89@gmail.com>
2022-10-23 08:47:14 -0400

    adds new connector class

---
+++ src/sample/dao/DBConnector.java
@@ -0,0 +1,29 @@
+package sample.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBConnector {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/client_schedule";
+    private static final String UNAME = "sqlUser";
+    private static final String PWORD = "Passw0rd!";
+
+    private static Connection conn;
+
+    public static void connect() throws SQLException {
+        conn = (Connection) DriverManager.getConnection(DB_URL, UNAME, PWORD);
+    }
+
+    public static void closeConnection() throws SQLException {
+        conn.close();
+    }
+
+    public static Statement newStatement() throws SQLException {
+        return conn.createStatement();
+    }
+
+
+
+}
