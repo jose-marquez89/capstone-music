fc15e69a98239a9e229f6b4b1ac9cd0170bb8dc1
jose-marquez89 <jlmarquez89@gmail.com>
2022-10-24 15:25:02 -0400

    removes initial focus from username textfield

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
         primaryStage.setTitle("Scheduler Pro");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        root.requestFocus();
     }
 
 
