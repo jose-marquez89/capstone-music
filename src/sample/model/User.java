f01412e0791c6a598e54951513d7e14f09cbde4e
jose-marquez89 <jlmarquez89@gmail.com>
2022-10-25 13:06:22 -0400

    simplifies user class

--- src/sample/model/User.java
+++ src/sample/model/User.java
@@ -3,20 +3,9 @@ package sample.model;
 import java.time.ZonedDateTime;
 
 public class User extends Person {
-    private String password;
-    private static int passwordRetrievalsLeft = 1;
 
     public User(int id, String name, ZonedDateTime createDate, String createdBy,
-                ZonedDateTime lastUpdate, String lastUpdatedBy, String password) {
+                ZonedDateTime lastUpdate, String lastUpdatedBy) {
         super(id, name, createDate, createdBy, lastUpdate, lastUpdatedBy);
-        this.password = password;
-    }
-
-    public String getPassword() {
-        // not great logic
-        if (passwordRetrievalsLeft-- > 0) {
-            return password;
-        }
-        return "";
     }
 }
