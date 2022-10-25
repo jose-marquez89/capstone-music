f7e1a03f0df8040eecff1f74bbdb7fea41afef20
jose-marquez89 <jlmarquez89@gmail.com>
2022-10-25 13:53:40 -0400

    adds notes for customer database fields

--- src/sample/controller/UserDashboardController.java
+++ src/sample/controller/UserDashboardController.java
@@ -1,4 +1,21 @@
 package sample.controller;
 
 public class UserDashboardController {
+    /*
+    TODO: add fields to show for customers
+    - customer_id
+    - customer_name
+    - address
+    - postal_code
+    - phone
+    - create_date
+    - created_by
+    - last_update
+    - last_updated_by
+    - division_id
+     */
+
+    /*
+    TODO: add fields to show for appointments
+     */
 }
