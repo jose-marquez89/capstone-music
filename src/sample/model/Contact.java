b1882c6a171f72b9633a10272e1ee92ec8079a56
jose-marquez89 <jlmarquez89@gmail.com>
2022-10-31 14:34:37 -0500

    creates contact class for display

---
+++ src/sample/model/Contact.java
@@ -0,0 +1,9 @@
+package sample.model;
+
+public class Contact extends Person {
+    private int id;
+    private String name;
+    public Contact(int id, String name) {
+        super(id, name);
+    }
+}
