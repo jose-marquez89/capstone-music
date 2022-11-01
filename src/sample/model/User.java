0dde23702be7ec867024771e15efb7bd57f62484
jose-marquez89 <jlmarquez89@gmail.com>
2022-11-01 08:52:49 -0500

    changes date types back to LocalDateTime

--- src/sample/model/User.java
+++ src/sample/model/User.java
@@ -1,11 +1,12 @@
 package sample.model;
 
+import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
 public class User extends Person {
 
-    public User(int id, String name, ZonedDateTime createDate, String createdBy,
-                ZonedDateTime lastUpdate, String lastUpdatedBy) {
+    public User(int id, String name, LocalDateTime createDate, String createdBy,
+                LocalDateTime lastUpdate, String lastUpdatedBy) {
         super(id, name, createDate, createdBy, lastUpdate, lastUpdatedBy);
     }
 }
