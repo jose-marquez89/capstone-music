a37e14e769ce8e565efae43163ca500fffa82d7a
jose-marquez89 <jlmarquez89@gmail.com>
2022-11-01 17:43:22 -0500

    adds initial LocalDateTime validation code

--- src/sample/utility/AppointmentValidator.java
+++ src/sample/utility/AppointmentValidator.java
@@ -1,5 +1,32 @@
 package sample.utility;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+
 public class AppointmentValidator {
-    // TODO: implement validation
+    public static boolean validateStartEnd(LocalDateTime start, LocalDateTime end) {
+        return start.isBefore(end);
+    }
+    public static boolean validateOverlap(LocalDateTime start, LocalDateTime end) {
+        LocalTime estStart, estEnd;
+        LocalTime businessStart = LocalTime.of(8, 0);
+        LocalTime businessEnd = LocalTime.of(22, 0);
+        boolean validated = false;
+
+        estStart = start
+                .atZone(ZoneId.systemDefault())
+                .withZoneSameInstant(ZoneId.of("America/New York"))
+                .toLocalTime();
+        estEnd = end
+                .atZone(ZoneId.systemDefault())
+                .withZoneSameInstant(ZoneId.of("America/New York"))
+                .toLocalTime();
+
+        if (estStart.compareTo(businessStart) >= 0 && estEnd.compareTo(businessEnd) <= 0) {
+            validated = true;
+        }
+
+        return validated;
+    }
 }
