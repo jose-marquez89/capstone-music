0db786bcd2b6456b921772a5d069a11e236f2e8a
jose-marquez89 <jlmarquez89@gmail.com>
2022-11-06 17:11:54 -0600

    adds log in methods and attributes to user class

--- src/sample/model/User.java
+++ src/sample/model/User.java
@@ -1,12 +1,22 @@
 package sample.model;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.ZonedDateTime;
 
 public class User extends Person {
+    private LocalDateTime logInDateTime = null;
 
     public User(int id, String name, LocalDateTime createDate, String createdBy,
                 LocalDateTime lastUpdate, String lastUpdatedBy) {
         super(id, name, createDate, createdBy, lastUpdate, lastUpdatedBy);
+    }
+
+    public LocalDateTime getLogInDateTime() {
+        return logInDateTime;
+    }
+
+    public void setLogInTime() {
+        logInDateTime = LocalDateTime.now();
     }
 }
