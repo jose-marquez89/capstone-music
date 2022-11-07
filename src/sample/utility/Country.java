8821d5ca217436abe0fbce6999d8e849029f15a8
jose-marquez89 <jlmarquez89@gmail.com>
2022-11-07 11:01:43 -0600

    adds country class

---
+++ src/sample/utility/Country.java
@@ -0,0 +1,10 @@
+package sample.utility;
+
+public class Country extends Location {
+    private int id;
+    private String name;
+
+    public Country(int id, String name) {
+        super(id, name);
+    }
+}
