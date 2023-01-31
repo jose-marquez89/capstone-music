e07323925d3b2a69e1e4bd929e95aae5ac53f965
jose-marquez89 <jlmarquez89@gmail.com>
2023-01-31 10:38:56 -0600

    modifies login controller to work with new DB

--- src/sample/controller/LogInController.java
+++ src/sample/controller/LogInController.java
@@ -11,12 +11,15 @@ import javafx.scene.control.*;
 import javafx.stage.Stage;
 import sample.dao.DBConnector;
 import sample.dao.Query;
+import sample.model.Manager;
 import sample.model.Schedule;
 import sample.model.User;
 import sample.utility.LogInLogger;
 
+import javax.xml.transform.Result;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
@@ -38,7 +41,7 @@ public class LogInController implements Initializable {
     @FXML private PasswordField passwordField;
     @FXML private Label zoneLabel;
     @FXML private Button logInButton;
-    private ResultSet queryResult;
+    private ResultSet queryResult, mgrResult;
     private Parent root;
     private Stage stage;
     private Scene scene;
@@ -57,10 +60,10 @@ public class LogInController implements Initializable {
         logInButton.setText(logInBtnText);
     }
     public void logIn(ActionEvent event) throws SQLException, IOException {
-        User queuedUser;
-        int id;
-        String name, createdBy, lastUpdatedBy;
-        LocalDateTime createdDate, lastUpdatedDate;
+        int id, storeId;
+        Manager sessionManager = null;
+        String name, createdBy, managerDetailsQ;
+        LocalDateTime startdate, enddate;
         String candidatePword = passwordField.getText();
         String candidateUname = usernameField.getText();
         ResourceBundle localeBundle = ResourceBundle.getBundle("Nat", Locale.getDefault());
@@ -73,24 +76,48 @@ public class LogInController implements Initializable {
         badLogin.setContentText(alertMessage);
 
         DBConnector.connect();
-        Query.runQuery("SELECT * FROM users WHERE user_name = '" + candidateUname + "';");
+        Query.runQuery("SELECT * FROM manager_detail WHERE user_name = '" + candidateUname + "';");
         queryResult = Query.getResults();
 
         if (queryResult.next()) {
             // we really don't need the User class
-            id = Integer.parseInt(queryResult.getString("user_id"));
+            id = Integer.parseInt(queryResult.getString("employee_id"));
             name = queryResult.getString("user_name");
-            createdDate = queryResult.getTimestamp("create_date")
-                    .toLocalDateTime();
-            lastUpdatedDate = queryResult.getTimestamp("create_date")
-                    .toLocalDateTime();
-            createdBy = queryResult.getString("created_by");
-            lastUpdatedBy = queryResult.getString("last_updated_by");
-            queuedUser = new User(id, name, createdDate,createdBy, lastUpdatedDate, lastUpdatedBy);
+            storeId = queryResult.getInt("store_id");
+
 
             if (candidatePword.equals(queryResult.getString("password"))) {
-                queuedUser.setLogInDateTime();
-                Schedule.setCurrentUser(queuedUser);
+                // create the session's manager
+                managerDetailsQ = """
+                        SELECT e.*,
+                            m.user_name,
+                            m.store_id
+                        FROM employee AS e
+                        JOIN manager_detail AS m
+                        ON e.id = m.employee_id
+                        WHERE e.id = ?;       
+                        """;
+
+                PreparedStatement managerPs = Query.pendingStatement(managerDetailsQ);
+                managerPs.setInt(1, id);
+                System.out.println("Currently attempting to log in employee id: " + id);
+
+                mgrResult = managerPs.executeQuery();
+
+                if (mgrResult.next()) {
+                    startdate = mgrResult.getTimestamp("start_date").toLocalDateTime();
+                    try {
+                        enddate = mgrResult.getTimestamp("end_date").toLocalDateTime();
+                    } catch (NullPointerException npe){
+                        enddate = null;
+                    }
+                    sessionManager = new Manager(id, startdate, enddate, candidateUname, storeId);
+                } else {
+                    // TODO: change to proper logging event and break
+                    System.out.println("Something's gone wrong with manager log in and DB");
+                }
+
+                // Schedule.setCurrentUser(queuedUser);
                 root = FXMLLoader.load(getClass().getResource("../view/user-dashboard.fxml"));
                 stage = (Stage)((Node)event.getSource()).getScene().getWindow();
                 scene = new Scene(root);
@@ -98,6 +125,8 @@ public class LogInController implements Initializable {
                 stage.setX(100.0);
                 stage.setY(5.0);
                 stage.show();
+
+                System.out.println("Login Successful for " + sessionManager.getUsername());
 
                 DBConnector.closeConnection();
                 LogInLogger.getLogger().info("Login succeeded for user candidate: " + candidateUname);
