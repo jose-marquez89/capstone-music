b05597c1b82f63bca03d2850cc56fcfefe1a4684
jose-marquez89 <jlmarquez89@gmail.com>
2023-02-01 19:14:29 -0600

    adds initial code

--- src/sample/controller/ManagerCmdCtrl.java
+++ src/sample/controller/ManagerCmdCtrl.java
@@ -1,4 +1,18 @@
 package sample.controller;
 
-public class ManagerCmdCtrl {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import sample.utility.Session;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ManagerCmdCtrl implements Initializable {
+    @FXML private Label managerLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        managerLabel.setText("Session Manager: " + Session.getManagerName());
+    }
 }
