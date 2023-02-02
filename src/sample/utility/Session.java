4fe8a490fbe4c529d5a5fa6c3d80767c42c798df
jose-marquez89 <jlmarquez89@gmail.com>
2023-02-01 19:15:20 -0600

    adds session utility class

---
+++ src/sample/utility/Session.java
@@ -0,0 +1,25 @@
+package sample.utility;
+
+import sample.model.Manager;
+import sample.model.SalesAssociate;
+
+public class Session {
+    private static Manager manager;
+    private static SalesAssociate salesAssociate;
+
+    public static void setManager(Manager manager) {
+        Session.manager = manager;
+    }
+
+    public static void setSalesAssociate(SalesAssociate salesAssociate) {
+        Session.salesAssociate = salesAssociate;
+    }
+
+    public static String getManagerName() {
+        return manager.getName();
+    }
+
+    public static String getManagerUserName() {
+        return manager.getUsername();
+    }
+}
