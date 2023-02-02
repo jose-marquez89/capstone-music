73d996a70089b8e35d0cb6a2cf055b3d3c88ca24
jose-marquez89 <jlmarquez89@gmail.com>
2023-02-02 06:29:32 -0600

    updates class field names

--- src/sample/model/SalesAssociate.java
+++ src/sample/model/SalesAssociate.java
@@ -4,8 +4,8 @@ import java.time.LocalDateTime;
 
 public class SalesAssociate extends Employee {
 
-    public SalesAssociate(int id, String name, LocalDateTime startdate, LocalDateTime enddate) {
-        super(id, name, startdate, enddate);
+    public SalesAssociate(int id, String name, LocalDateTime startDate, LocalDateTime endDate) {
+        super(id, name, startDate, endDate);
     }
 
     public void terminate() {
