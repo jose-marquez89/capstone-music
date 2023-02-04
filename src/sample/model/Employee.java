d2b1a45daec8a688c0795bf360c9986f7b3f3034
jose-marquez89 <jlmarquez89@gmail.com>
2023-02-03 21:33:57 -0600

    removes id which was moved to Person class

--- src/sample/model/Employee.java
+++ src/sample/model/Employee.java
@@ -3,13 +3,11 @@ package sample.model;
 import java.time.LocalDateTime;
 
 abstract public class Employee extends Person {
-    private int id;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
 
     public Employee(int id, String name, LocalDateTime startDate, LocalDateTime endDate) {
-        super(name);
-        this.id = id;
+        super(id, name);
         this.startDate = startDate;
         this.endDate = endDate;
     }
