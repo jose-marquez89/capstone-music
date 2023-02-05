aa4ea66e7f80742712c3b9398c66ae55a89f4bac
jose-marquez89 <jlmarquez89@gmail.com>
2023-02-05 12:47:42 -0600

    adds new validators for phone and email

--- src/sample/utility/TextInputValidator.java
+++ src/sample/utility/TextInputValidator.java
@@ -1,2 +1,23 @@
-package sample.utility;public class TextInputValidator {
+package sample.utility;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TextInputValidator {
+
+    public static boolean validateEmail(String email) {
+        Pattern ptn = Pattern.compile("[\\w\\d\\\\.]+@[\\w\\d]+(\\.\\w+)+");
+        Matcher mo = ptn.matcher(email);
+        boolean isMatch = mo.matches();
+
+        return isMatch;
+    }
+
+    public static boolean validatePhone(String phone) {
+        Pattern ptn = Pattern.compile("\\(\\d{3}\\)\\s\\d{3}\\-\\d{4}");
+        Matcher mo = ptn.matcher(phone);
+        boolean isMatch = mo.matches();
+
+        return isMatch;
+    }
 }
