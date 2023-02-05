28d91f44fa698997de9155437938d61deb201481
jose-marquez89 <jlmarquez89@gmail.com>
2023-02-04 22:45:42 -0600

    adds initial switching code

--- src/sample/controller/InvMgmtCtrl.java
+++ src/sample/controller/InvMgmtCtrl.java
@@ -1,4 +1,29 @@
 package sample.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class InvMgmtCtrl {
+    private Parent root;
+    private Stage stage;
+    private Scene scene;
+
+    public void switchForms(ActionEvent e, String formName, String title) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../view/" + formName));
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        stage.setTitle(title);
+        scene = new Scene(root);
+        stage.setScene(scene);
+        root.requestFocus();
+        stage.show();
+    }
+    public void toManagerConsole(ActionEvent event) throws IOException {
+        switchForms(event, "manager-cmd-ctrl.fxml", "Manager Console");
+    }
 }
