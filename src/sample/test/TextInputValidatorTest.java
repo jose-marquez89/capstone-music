ee002abd68b6a38564242b528fb20848b4a016e2
jose-marquez89 <jlmarquez89@gmail.com>
2023-02-05 12:48:00 -0600

    adds new tests for phone and email validators

--- src/sample/test/TextInputValidatorTest.java
+++ src/sample/test/TextInputValidatorTest.java
@@ -1,4 +1,41 @@
+package sample.test;
+
+import org.junit.jupiter.api.Test;
+import sample.utility.TextInputValidator;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TextInputValidatorTest {
-  
+
+    @Test
+    void validateEmail() {
+        String testEmail1, testEmail2, testEmail3, testEmail4;
+
+        testEmail1 = "jose.marquez@brown.co.uk";
+        testEmail2 = "robert_johnson34@yahoo.com";
+        testEmail3 = "invalid_email#email.com";
+        testEmail4 = "notAn Emal At All";
+
+        assertTrue(TextInputValidator.validateEmail(testEmail1));
+        assertTrue(TextInputValidator.validateEmail(testEmail2));
+        assertFalse(TextInputValidator.validateEmail(testEmail3));
+        assertFalse(TextInputValidator.validateEmail(testEmail4));
+    }
+
+    @Test
+    void validatePhone() {
+        String testPhone1, testPhone2, testPhone3, testPhone4, testPhone5;
+
+        testPhone1 = "(210) 456-6785";
+        testPhone2 = "(345 345-4955";
+        testPhone3 = "345-3456";
+        testPhone4 = "123 234 3456";
+        testPhone5 = "1233455432";
+
+        assertTrue(TextInputValidator.validatePhone(testPhone1));
+        assertFalse(TextInputValidator.validatePhone(testPhone2));
+        assertFalse(TextInputValidator.validatePhone(testPhone3));
+        assertFalse(TextInputValidator.validatePhone(testPhone4));
+        assertFalse(TextInputValidator.validatePhone(testPhone5));
+    }
 }
