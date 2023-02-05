8e10ee527e6f96e94077d4814e0b22d0d35faef9
jose-marquez89 <jlmarquez89@gmail.com>
2023-02-04 22:48:32 -0600

    adds initial switching code

--- src/sample/controller/NewCustomerCtrl.java
+++ src/sample/controller/NewCustomerCtrl.java
@@ -1,4 +1,31 @@
 package sample.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.IOException;
+
 public class NewCustomerCtrl {
+    private Parent root;
+    private Stage stage;
+    private Scene scene;
+
+    public void switchForms(ActionEvent e, String formName, String title) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../view/" + formName));
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        stage.setTitle(title);
+        scene = new Scene(root);
+        stage.setScene(scene);
+        root.requestFocus();
+        stage.show();
+    }
+
+    public void cancel(ActionEvent e) throws IOException {
+        switchForms(e, "pos.fxml", "POS");
+    }
 }
