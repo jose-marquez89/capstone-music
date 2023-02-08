c484fdf59ad43230e9d4c453ad5a9ab8e17b0f5a
jose-marquez89 <jlmarquez89@gmail.com>
2023-02-08 10:49:22 -0600

    adds method to pass an order to the configuration screen

--- src/sample/controller/ReturnConfigCtrl.java
+++ src/sample/controller/ReturnConfigCtrl.java
@@ -1,4 +1,11 @@
 package sample.controller;
 
+import sample.model.Order;
+
 public class ReturnConfigCtrl {
+    private Order selectedOrder;
+
+    public void setSelectedOrder(Order targetOrder) {
+        selectedOrder = targetOrder;
+    }
 }
