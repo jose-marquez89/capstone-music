04acd962dd258e0a1d448157d74e2d3b86511445
jose-marquez89 <jlmarquez89@gmail.com>
2023-02-11 12:47:37 -0600

    adds initial controller methods

---
+++ src/sample/controller/AddServiceCtrl.java
@@ -0,0 +1,30 @@
+package sample.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AddServiceCtrl {
+    private Parent root;
+    private Stage stage;
+    private Scene scene;
+
+    public void cancel(ActionEvent e) throws IOException {
+        switchForms(e, "manage-inventory.fxml", "Inventory Management");
+    }
+
+    public void switchForms(ActionEvent e, String formName, String title) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../view/" + formName));
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        stage.setTitle(title);
+        scene = new Scene(root);
+        stage.setScene(scene);
+        root.requestFocus();
+        stage.show();
+    }
+}
