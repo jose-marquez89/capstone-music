40466a2209f4a66365a162244ed21e41626b7345
jose-marquez89 <jlmarquez89@gmail.com>
2023-02-11 09:10:42 -0600

    adds new validators for numeric input

--- src/sample/utility/TextInputValidator.java
+++ src/sample/utility/TextInputValidator.java
@@ -5,6 +5,22 @@ import java.util.regex.Pattern;
 
 public class TextInputValidator {
 
+    public static boolean validatePrice(String price) {
+        Pattern ptn = Pattern.compile("^\\d+\\.\\d{2}$");
+        Matcher mo = ptn.matcher(price);
+        boolean isMatch = mo.matches();
+
+        return isMatch;
+    }
+
+    public static boolean validateInteger(String intInput) {
+        Pattern ptn = Pattern.compile("^\\d+$");
+        Matcher mo = ptn.matcher(intInput);
+        boolean isMatch = mo.matches();
+
+        return isMatch;
+    }
+
     public static boolean validateEmail(String email) {
         Pattern ptn = Pattern.compile("[\\w\\d\\\\.]+@[\\w\\d]+(\\.\\w+)+");
         Matcher mo = ptn.matcher(email);
