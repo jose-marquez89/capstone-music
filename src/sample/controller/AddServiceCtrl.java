253e54ae408dbd08f4b6a4b1b5b9891e895e7da8
jose-marquez89 <jlmarquez89@gmail.com>
2023-02-12 15:03:53 -0600

    adds method for saving new service

--- src/sample/controller/AddServiceCtrl.java
+++ src/sample/controller/AddServiceCtrl.java
@@ -12,6 +12,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.SelectionModel;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import sample.dao.DBConnector;
@@ -19,14 +20,19 @@ import sample.dao.Query;
 import sample.model.Product;
 import sample.utility.Notification;
 import sample.utility.Session;
+import sample.utility.TextInputValidator;
 
+import javax.xml.validation.Validator;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class AddServiceCtrl implements Initializable {
+    @FXML TextField nameField;
+    @FXML TextField priceField;
     @FXML TableView<Product> allProductsTable;
     @FXML TableColumn<Product, String> productNameCol;
     @FXML TableView<Product> associatedProductsTable;
@@ -118,6 +124,71 @@ public class AddServiceCtrl implements Initializable {
         switchForms(e, "manage-inventory.fxml", "Inventory Management");
     }
 
+    public void submitNew(ActionEvent e) throws SQLException, IOException {
+        int newId;
+        double newPrice;
+        String newName, serviceQuery, associationQuery, txtPrice, newIdQuery;
+        PreparedStatement servicePs, associationPs;
+        ResultSet idRs;
+
+        serviceQuery = """
+                INSERT INTO service (name, price)
+                VALUES (?, ?);
+                """;
+
+        newIdQuery = """
+                SELECT MAX(id) max_id
+                FROM service;
+                """;
+
+        associationQuery = """
+                INSERT INTO service_application (service_id, product_id)
+                VALUES (?, ?);
+                """;
+
+        newName = nameField.getText();
+        txtPrice = priceField.getText();
+
+        if (newName.equals("")) {
+            Notification.blankName();
+            return;
+        }
+
+        if (!TextInputValidator.validatePrice(txtPrice)) {
+            Notification.badNumericInput();
+            return;
+        } else {
+            newPrice = Double.parseDouble(txtPrice);
+        }
+
+        DBConnector.connect();
+        servicePs = Query.pendingStatement(serviceQuery);
+        servicePs.setString(1, newName);
+        servicePs.setDouble(2, newPrice);
+        servicePs.executeUpdate();
+
+        // not the best way to do this, may need to be refactored to properly scale
+        Query.runQuery(newIdQuery);
+        idRs = Query.getResults();
+
+        if (idRs.next()) {
+            newId = idRs.getInt("max_id");
+        } else {
+            throw new RuntimeException("Database is not populated!");
+        }
+
+        associationPs = Query.pendingStatement(associationQuery);
+
+        for (Product p : assProdsContainer) {
+            associationPs.setInt(1, newId);
+            associationPs.setInt(2, p.getId());
+            associationPs.addBatch();
+        }
+
+        associationPs.executeBatch();
+        Notification.genericInfoWait("New Service", "The new service, including associated products, has been successfully added.");
+        switchForms(e, "manage-inventory.fxml", "Manage Inventory");
+    }
     public void switchForms(ActionEvent e, String formName, String title) throws IOException {
         root = FXMLLoader.load(getClass().getResource("../view/" + formName));
         stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
